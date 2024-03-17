b14c6206180b74a1586486e65edc6caecc19119b
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-03-17 19:02:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	for(let i=0;i<str.length;i++){
+		if(i-1>=0 && i+1 < str.length && str[i] != str[i-1] && str[i] != str[i+1]){
+			return str[i]
+		}
+	}
+	return null
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
